ea84a305aa9e6a309228f2e7b795ae4c2ffeb66b
Younes Arroubi <yarroubi@e1r4p12.1337.ma>
2021-05-25 14:53:47 +0100

    strndup

--- ft_string/ft_string.h
+++ ft_string/ft_string.h
@@ -6,7 +6,7 @@
 /*   By: youness <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/05 14:22:12 by youness           #+#    #+#             */
-/*   Updated: 2021/05/05 14:29:40 by youness          ###   ########.fr       */
+/*   Updated: 2021/05/25 14:53:23 by yarroubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 char	*ft_strdup(const char *s1);
+char	*ft_strndup(const char *str, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	**ft_split(char const *s, char c);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
